124958b20fa67175d584011e3edc34b77375b9f0
M Niaz Malik <niazk9770@gmail.com>
2026-07-01 15:21:49 +0500

    Update save.php

--- save.php
+++ save.php
@@ -1,32 +1,3 @@
-// <?php
-
-// include "db.php";
-
-// $meeting=$_POST['meeting'];
-// $phone=$_POST['phone'];
-// $instagram=$_POST['instagram'];
-// $snapchat=$_POST['snapchat'];
-// $whatsapp=$_POST['whatsapp'];
-// $message=$_POST['message'];
-
-// $sql="INSERT INTO responses
-// (meeting,phone,instagram,snapchat,whatsapp,message)
-// VALUES
-// ('$meeting','$phone','$instagram','$snapchat','$whatsapp','$message')";
-
-// if(mysqli_query($conn,$sql)){
-
-// echo "Success";
-
-// }else{
-
-// echo "Failed";
-
-// }
-
-// ?>
-
-
 <?php
 
 include "db.php";
